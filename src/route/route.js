99d5e0c40d119c620c2ccc2f0a53edfd11cd792a
Eloic <99272320+Eloic1592@users.noreply.github.com>
2023-02-03 10:13:56 +0300

    Update route.js

--- src/route/route.js
+++ src/route/route.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 const useLink = "https://projetcloudenchere-production.up.railway.app/";
-// const useLink = "http://localhost:8080/";
 class Link {
     getUseLink() {
         return useLink;
