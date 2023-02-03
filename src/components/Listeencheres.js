c1807e1bb766a0403e7b46ae1a30a247334b834b
Eloic <99272320+Eloic1592@users.noreply.github.com>
2023-02-03 12:07:38 +0300

    Update Listeencheres.js

--- src/components/Listeencheres.js
+++ src/components/Listeencheres.js
@@ -1,4 +1,4 @@
-import React, { Component, useEffect, useState } from "react";
+import React, { useState } from "react";
 import './assets/images/favicon.ico';
 import './assets/css/graindashboard.css';
 import Header from "./Header";
